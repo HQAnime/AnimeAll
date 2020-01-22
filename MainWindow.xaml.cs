69bc3bc5789f2166565cab6ea82bf1e4f0422197
Yiheng (NUC) <development.henryquan@gmail.com>
2020-01-22 14:56:36 +1100

    a bacis webview

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Toolkit.Wpf.UI.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Windows.UI.ViewManagement;
 
 namespace AnimeAll
 {
@@ -22,6 +24,31 @@ namespace AnimeAll
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void weball_NewWindowRequested(object sender, Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT.WebViewControlNewWindowRequestedEventArgs e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+
+        private void weball_ContainsFullScreenElementChanged(object sender, object e)
+        {
+            if ((sender as WebView).ContainsFullScreenElement)
+            {
+                rootWindow.Visibility = Visibility.Collapsed;
+                rootWindow.WindowStyle = WindowStyle.None;
+                rootWindow.ResizeMode = ResizeMode.NoResize;
+                rootWindow.WindowState = WindowState.Maximized;
+                rootWindow.Topmost = true;
+                rootWindow.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                rootWindow.WindowStyle = WindowStyle.SingleBorderWindow;
+                rootWindow.WindowState = WindowState.Normal;
+                rootWindow.ResizeMode = ResizeMode.CanResize;
+                rootWindow.Topmost = false;
+            }
         }
     }
 }
