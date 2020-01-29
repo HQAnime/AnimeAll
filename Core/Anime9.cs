d373a8ad5730edb3ab5f9ceb7d6db9b0ca9ffdcf
Yiheng (ThinkPad) <development.henryquan@gmail.com>
2020-01-29 19:42:21 +1100

    added some classes

---
+++ Core/Anime9.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Toolkit.Wpf.UI.Controls;
+
+namespace AnimeAll.Core
+{
+    [Obsolete]
+    class Anime9 : WebView
+    {
+    }
+}
