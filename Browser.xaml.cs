21b5e981c07702340f452c153373275fdb7aa4c3
Yiheng (ThinkPad) <development.henryquan@gmail.com>
2020-01-29 20:32:18 +1100

    move logic to browser

--- Browser.xaml.cs
+++ Browser.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using AnimeAll.Core;
+using Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT;
 using System.Windows;
 
 namespace AnimeAll
@@ -16,6 +17,36 @@ namespace AnimeAll
             anime = new Core.AnimeAll(site);
         }
 
+        private void weball_ContainsFullScreenElementChanged(object sender, object e)
+        {
+            if ((sender as WebView).ContainsFullScreenElement)
+            {
+                // Fullscreen
+                rootWindow.Visibility = Visibility.Collapsed;
+                rootWindow.WindowStyle = WindowStyle.None;
+                rootWindow.ResizeMode = ResizeMode.NoResize;
+                rootWindow.WindowState = WindowState.Maximized;
+                rootWindow.Topmost = true;
+                rootWindow.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                // Exit Fullscreen
+                rootWindow.WindowStyle = WindowStyle.SingleBorderWindow;
+                rootWindow.WindowState = WindowState.Normal;
+                rootWindow.ResizeMode = ResizeMode.CanResize;
+                rootWindow.Topmost = false;
+            }
+        }
 
+        private void weball_FrameDOMContentLoaded(object sender, WebViewControlDOMContentLoadedEventArgs e)
+        {
+            anime.InjectJS();
+        }
+
+        private void weball_DOMContentLoaded(object sender, WebViewControlDOMContentLoadedEventArgs e)
+        {
+            anime.InjectJS();
+        }
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using Microsoft.Toolkit.Wpf.UI.Controls;
 using System;
 using System.Windows;
+using AnimeAll.Core;
 
 namespace AnimeAll
 {
@@ -10,52 +11,15 @@ namespace AnimeAll
     /// </summary>
     public partial class MainWindow : Window
     {
-        private readonly string[] script = new string[]
-        {
-            @"$('.image-with-text, .ad-bebi, .bebi-icon-hover, img[border]').remove();",
-        };
-
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void weball_ContainsFullScreenElementChanged(object sender, object e)
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-            if ((sender as WebView).ContainsFullScreenElement)
-            {
-                // Fullscreen
-                rootWindow.Visibility = Visibility.Collapsed;
-                rootWindow.WindowStyle = WindowStyle.None;
-                rootWindow.ResizeMode = ResizeMode.NoResize;
-                rootWindow.WindowState = WindowState.Maximized;
-                rootWindow.Topmost = true;
-                rootWindow.Visibility = Visibility.Visible;
-            }
-            else
-            {
-                // Exit Fullscreen
-                rootWindow.WindowStyle = WindowStyle.SingleBorderWindow;
-                rootWindow.WindowState = WindowState.Normal;
-                rootWindow.ResizeMode = ResizeMode.CanResize;
-                rootWindow.Topmost = false;
-            }
-        }
-
-        private void injectJS(string msg)
-        {
-            weball.InvokeScriptAsync("eval", this.script);
-            Console.WriteLine("Script has been invoked, " + msg);
-        }
-
-        private void weball_FrameDOMContentLoaded(object sender, WebViewControlDOMContentLoadedEventArgs e)
-        {
-            injectJS("FrameDOM");
-        }
-
-        private void weball_DOMContentLoaded(object sender, WebViewControlDOMContentLoadedEventArgs e)
-        {
-            injectJS("OMContent");
+            var browser = new Browser(Website.AnimeOne);
+            browser.Show();
         }
     }
 }
